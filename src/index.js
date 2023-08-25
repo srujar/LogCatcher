9e1838bc665aaaf25fcf70603aeb8135f118a486
srujankritya <srujan.kritya@gmail.com>
2023-08-25 22:30:18 +0530

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 require('./db/mongodb');
 require('./models/Logger');
 const runScript = require('./db/deployment_Script');
- 
+
 const LogRoute = require('./routes/log');
 
 const app = express();
 app.use(express.json());
+
+
+app.use(cors());
 
 app.use('/', express.static(path.join(__dirname, '../LogCatcher/dist/log-catcher/')));
 app.use(LogRoute);
